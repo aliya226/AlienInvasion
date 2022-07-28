5b4797329261d340912ec5a058cda4822ca4af6d
Aliya226 <aliya0802@gmail.com>
2022-07-28 16:15:07 -0700

    first completed game stucture

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,21 +1,37 @@
+"""Alien Invasion class"""
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
-    """Overall class to manage games asset and behavior""" 
-    
+
+    """Overall class to manage games asset and behavior"""
+
     def __init__(self):
         pygame.init()
-        
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        
-        def run_game(self):
-            """Main loop for the game"""
-            while True:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        sys.exit()
-                
-                pygame.display.flip()
-        
+        self.ship = Ship(self)
+        #set backround color
+        self.bg_color = (230,230,230)
+
+    def run_game(self):
+
+        """Main loop for the game"""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            #redraw the screen during each loop
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+                #Make most recent screen visible
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    AI = AlienInvasion()
+    AI.run_game()
---
+++ settings.py
@@ -0,0 +1,9 @@
+"""Settings class"""
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+    def __init__(self):
+        """Inialize the games settings"""
+        #screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
