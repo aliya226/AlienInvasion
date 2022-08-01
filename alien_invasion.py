652b8a5855da5a76040d5ebc865f01e606f2f404
Aliya226 <aliya0802@gmail.com>
2022-08-01 12:48:39 -0400

    created keybinds to work with ship movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,52 +11,54 @@ class AlienInvasion:
 
     def __init__(self):
         pygame.init()
-
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width,self.settings.screen_height))
-
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+
         self.ship = Ship(self)
-        #set backround color
-        self.bg_color = (230,230,230)
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
-
         """Main loop for the game"""
         while True:
             self._check_events()
-            self.ship_update()
             self._update_screen()
+            self.ship.update()
 
     def _check_events(self):
-        """Respond to keypresses and mouse events"""
+        """Respond to keypresses"""
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                #Move the ship to the right
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                #Move the ship up
-                if event.type == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
 
+    def _check_keydown_events(self, event):
+        """Respond to keypresses"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases"""
+        if event.type == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen and flip to a new screen"""
-        #redraw the screen during each loop
         self.screen.fill(self.settings.bg_color)
         self.ship.draw_ship()
-        #Make most recent screen visible
         pygame.display.flip()
 
 if __name__ == '__main__':
--- ship.py
+++ ship.py
@@ -1,32 +1,35 @@
 """Ship class"""
 import pygame
 
-
 class Ship:
     """A class to manage the ship"""
 
-    def __init__(self, ai_game):
+    def __init__(self,ai_game):
         """Inialize the ship and its starting point"""
+        self.moving_right = False
+        self.moving_left = False
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen_rect()
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect
         self.image = pygame.image.load('images/spaceship.png')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen
-        self.rect.midbottom = self.screen.rect.midbottom
+        self.rect.midbottom = self.screen_rect.midbottom
 
-        #Movement flag
-        self.moving_right = False
-        self.moving_left = False
+        self.x = float(self.rect.x)
 
     def update(self):
         """Update the ships position based on the movement flag"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+            #Update rect object
+            self.rect.x = self.x
 
 
     def draw_ship(self):
